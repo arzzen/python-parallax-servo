ae8a92c43c6b6f36cfa392a3ddc268798a9ac979
lukas.mestan <lukas.mestan@gmail.com>
2014-03-15 20:01:50 +0100

    init

---
+++ servo.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+import time
+import sys
+sys.path.append('modules')
+import ParallaxServoController as PSC
+import ParallaxServoHandler as PSH
+
+
+rate = 3
+servo = PSC.ParallaxServoController('COM7', baudrate=2400)
+handler = PSH.ParallaxServoHandler(servo)
+
+servo.setHomePosition(rate)
+time.sleep(7)
+
+handler.setX(1)
+handler.setY(1)
+handler.setZ(1)
+
+handler.setLengthA(15)
+handler.setLengthB(20)
+handler.setLengthC(10)
+###
+handler.findPosition(x, y, z)
+
+
+
+
